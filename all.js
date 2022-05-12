bc6be84ce9369414fc3eb928023cf0806bef5ffa
Nicholas Berlette <nick@berlette.com>
2022-05-12 20:49:43 +0000

    ✨ create all.js for islands-style projects

---
+++ all.js
@@ -0,0 +1,11 @@
+/** @version {1.7.2} */
+/** @type {PrettierConfig} */
+module.exports = {
+	...(require('@brlt/prettier')),
+	overrides: [
+		...(require('@brlt/prettier/svelte').overrides || []),
+		...(require('@brlt/prettier/react').overrides || []),
+		...(require('@brlt/prettier/bash').overrides || []),
+		...(require('@brlt/prettier/vue').overrides || []),
+	],
+};
